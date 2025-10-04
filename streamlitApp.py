656a5b59f0f6dae071389a622054eb0b075116f8
Loehnert12 <deloehnert12@gmail.com>
2025-10-04 10:24:38 -0400

    Updated Streamlit app

--- streamlitApp.py
+++ streamlitApp.py
@@ -1,17 +1,12 @@
 import streamlit as st
 from datetime import date
 
-st.set_page_config(
-    page_title = "Date Difference Calulator"
-)
+st.set_page_config(page_title = "Date Difference Calulator")
 
-st.write("""
-# This is my basic application!""")
-st.write("""
-#### Lets figure out a day difference calculation.          
-         """)
+st.header("This is my basic application!")
+st.subheader("Lets figure out a day difference calculation.")
 
-st.header('Select your starting date.')
+st.header("Select your starting date.")
 
 start_date = st.date_input(
     "Pick a start date",
@@ -26,13 +21,15 @@ st.write(f"Start date: {start_date:%B %d, %Y}")
 end_date = st.date_input(
     "Pick an end date",
     value = date.today(),
-    min_value = start_date,
+    min_value = date(2000, 1, 1),
+    max_value = date (2100, 12, 31),
     key = "end_date"
 )
 
 st.write(f"End date: {end_date:%B %d, %Y}")
 
-if end_date and start_date:
+if start_date > end_date:
+    st.error("The start date has to be before the end date. Please double check the dates. :)")
+else:
     day_diff = (end_date - start_date).days
-
-st.header(f"It has been {day_diff} days since {start_date:%B %d, %Y}")
+    st.header(f"It has been {day_diff} days since {start_date:%B %d, %Y}")
